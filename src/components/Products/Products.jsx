6987fc0677a2eaf8c8bec4a69e0526ea275583cb
Ali <alimoosabadi326@gmail.com>
2023-09-10 19:43:11 +0330

    adding the basic redux features to the main component of products(Products.jsx and product.jsx) and disable context in these components

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,32 +1,46 @@
-import React, { useContext } from "react";
+import { useEffect } from "react";
 import Product from "./Product/Product";
 import Loading from "../Loading/Loading";
-import { appContext } from "../../context/appContext";
-
+import { useDispatch } from "react-redux";
+import { fetchProductsData } from "../../Redux/productsSlice/productsSlice";
+import { useSelector } from "react-redux";
 const Products = () => {
-  let { products } = useContext(appContext);
-  let markUp = <Loading />;
-  if (products.length) {
-    let productItems = products.map((product) => (
-      <Product key={product.id} {...product} />
-    ));
-    markUp = (
-      <div className="container mb-5">
-        <div className="row gy-5 p-sm-2 p-5">{productItems}</div>
-        <footer className="mt-3 text-center">
-          Developed with Love by{" "}
-          <a
-            href="https://github.com/AliMsdy"
-            target="blank"
-            className="text-decoration-none"
-          >
-            AliMoosabadi
-          </a>
-        </footer>
+  const { loading, products, error } = useSelector((state) => state);
+  let dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProductsData());
+  }, [dispatch]);
+
+  return (
+    <div
+      style={{ height: "calc(100vh - 130px)" }}
+      className=" container mb-5 d-flex flex-column"
+    >
+      <div className="row gy-5 p-sm-2 p-5 text-center mb-5">
+        {loading ? (
+          <Loading />
+        ) : error ? (
+          <p className="text-danger">
+            {error.message}
+            <div>try again later</div>
+          </p>
+        ) : (
+          products.map((product) => <Product key={product.id} {...product} />)
+        )}
       </div>
-    );
-  }
-  return markUp;
+      <footer className="mt-auto text-center p-4">
+        Developed by{" "}
+        <a
+          href="https://github.com/AliMsdy"
+          target="blank"
+          className="text-decoration-none"
+        >
+          AliMoosabadi
+        </a>
+      </footer>
+    </div>
+  );
 };
 
 export default Products;
--- src/index.jsx
+++ src/index.jsx
@@ -6,11 +6,15 @@ import "./index.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 library.add(fas);
+import { Provider } from "react-redux";
+import store from "./Redux/store";
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
